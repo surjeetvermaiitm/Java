7d281ba0deee4a24d70b2285b697826edca4f14a
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-02-07 11:07:49 +0530

    6-feb

--- Java_Sys_Design/src/Main.java
+++ Java_Sys_Design/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        String a="sur";
+        System.out.println(100+99+a);
     }
 }
